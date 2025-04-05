52dce1977127a5ac7ea8d500a3813b01ebe008b0
yeonupark <idepix@naver.com>
2025-04-05 21:47:50 +0900

    onboarding-problem4

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,26 @@
 package onboarding
 
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+    val result = StringBuilder()
+    for (c in word) {
+        result.append(
+            when {
+                c == ' ' -> ' '
+                else -> convert(c)
+            }
+        )
+    }
+    return result.toString()
+}
+
+fun convert(letter: Char): Char {
+    return when {
+        letter.isUpperCase() -> (65 + 26 -letter.code + 64).toChar()
+        letter.isLowerCase() -> (97 + 26 -letter.code + 96).toChar()
+        else -> letter
+    }
+}
+
+fun main() {
+    print(solution4("I love you"))
 }
