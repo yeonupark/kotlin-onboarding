b6c7fb0f3ab6ca2b1455d6d6817f58172d073354
yeonupark <idepix@naver.com>
2025-04-05 20:24:14 +0900

    onboarding-problem2

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -1,5 +1,32 @@
 package onboarding
 
 fun solution2(cryptogram: String): String {
-    TODO("프로그램 구현")
+    val result = simplify(cryptogram)
+    return result
+}
+
+fun simplify(cryptogram: String): String {
+
+    val result = ArrayDeque<Char>()
+    var temp = cryptogram[0]
+    result.addLast(temp)
+
+    for (i in 1 until cryptogram.length) {
+        when {
+            result.lastOrNull() == cryptogram[i] -> {
+                temp = result.last()
+                result.removeLast()
+            }
+
+            cryptogram[i] != temp -> {
+                result.addLast(cryptogram[i])
+            }
+        }
+    }
+
+    return result.joinToString("")
+}
+
+fun main() {
+    println(solution2("browoanooommnaon"))
 }
