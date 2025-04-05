633250c99719b69ab0876d7f4c02d865c3a462fe
yeonupark <idepix@naver.com>
2025-04-05 22:22:07 +0900

    onboarding-problem5

--- src/main/kotlin/onboarding/Problem5.kt
+++ src/main/kotlin/onboarding/Problem5.kt
@@ -1,5 +1,22 @@
 package onboarding
 
 fun solution5(money: Int): List<Int> {
-    TODO("프로그램 구현")
+    return divide(money, 50000)
+}
+
+fun divide(money: Int, divider: Int, result: MutableList<Int> = mutableListOf()): MutableList<Int> {
+    result.add(money/divider)
+    if (divider == 10) {
+        result.add(money%divider)
+        return result
+    }
+    return if (divider.toString()[0] == '5') {
+        divide(money % divider, divider/5, result)
+    } else {
+        divide(money % divider, divider/2, result)
+    }
+}
+
+fun main() {
+    print(solution5(50237))
 }
