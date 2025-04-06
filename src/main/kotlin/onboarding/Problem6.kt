efdffa5b15ef9adb363f6c2c0e304d542d694289
yeonupark <idepix@naver.com>
2025-04-06 11:16:42 +0900

    onboarding-problem6

--- src/main/kotlin/onboarding/Problem6.kt
+++ src/main/kotlin/onboarding/Problem6.kt
@@ -1,5 +1,50 @@
 package onboarding
 
 fun solution6(forms: List<List<String>>): List<String> {
-    TODO("프로그램 구현")
+    val stored = buildSubstringMap(forms)
+    return findDuplicatedEmails(forms, stored).sorted()
+}
+
+fun buildSubstringMap(forms: List<List<String>>): MutableMap<String, MutableList<Int>> {
+
+    val stored = mutableMapOf<String, MutableList<Int>>()
+
+    for (i in 0 ..forms.lastIndex) {
+        val (_, nickname) = forms[i]
+        for (j in 0 until nickname.length-1) {
+            addSubstring(nickname.substring(j, j+2), i, stored)
+        }
+    }
+
+    return stored
+}
+
+fun addSubstring(substring: String, idx: Int, stored: MutableMap<String, MutableList<Int>>)  {
+
+    when {
+        substring in stored -> stored[substring]?.add(idx)
+        else -> stored[substring] = mutableListOf(idx)
+    }
+}
+
+fun findDuplicatedEmails(forms: List<List<String>>, stored: MutableMap<String, MutableList<Int>>): HashSet<String> {
+    val duplicatedEmails = HashSet<String>()
+    for (substring in stored) {
+        if (substring.value.size > 1) {
+            for (idx in substring.value) {
+                duplicatedEmails.add(forms[idx][0])
+            }
+        }
+    }
+    return duplicatedEmails
+}
+
+fun main() {
+    solution6(listOf(
+        listOf("jm@email.com", "제이엠"),
+        listOf("jason@email.com", "제이슨"),
+        listOf("woniee@email.com", "워니"),
+        listOf("mj@email.com", "엠제이"),
+        listOf("nowm@email.com", "이제엠")
+    ))
 }
